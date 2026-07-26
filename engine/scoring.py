db63535ae97caaf7b5a019ba00a993bc364ce774
adamtall890-cmd <adamtall890@gmail.com>
2026-07-26 19:05:51 +0000

    Update scoring.py

--- engine/scoring.py
+++ engine/scoring.py
@@ -1,1 +1,33 @@
+def score_sources(results):
 
+    score = 0
+
+    for r in results:
+
+        url = r.get("url", "").lower()
+
+        if "wikipedia.org" in url:
+            score += 20
+
+        elif "larousse.fr" in url:
+            score += 20
+
+        elif ".gouv.fr" in url:
+            score += 40
+
+        elif ".gov" in url:
+            score += 40
+
+        elif ".edu" in url:
+            score += 35
+
+        elif "reuters.com" in url:
+            score += 40
+
+        elif "apnews.com" in url:
+            score += 40
+
+        else:
+            score += 5
+
+    return min(score, 100)
