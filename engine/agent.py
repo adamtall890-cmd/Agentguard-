1e81d2d163fc699aea9d95d7cde3bb1001f3ec91
adamtall890-cmd <adamtall890@gmail.com>
2026-07-27 15:29:38 +0000

    Update agent.py

--- engine/agent.py
+++ engine/agent.py
@@ -1,1 +1,11 @@
+def execute_workflow(workflow: str):
+    """
+    Simule un agent IA.
+    L'agent annonce toujours SUCCESS.
+    """
 
+    return {
+        "workflow": workflow,
+        "agent_status": "SUCCESS",
+        "message": "Task completed successfully."
+    }
